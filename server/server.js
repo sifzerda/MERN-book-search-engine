001f320401c1fd9521cec05407b32734c446ee70
Troy <tydamon@hotmail.com>
2024-05-01 17:05:52 +1000

    added queries and mutations, updated package.json files, search engine working

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+// added:
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
